7a4f28e00395fe6a24a40bea7af8ec7f8ec5d9e3
cavasini <cavasini.dev@gmail.com>
2024-02-16 22:14:44 -0300

    Código Final

--- Codigo_Fonte.py
+++ Codigo_Fonte.py
@@ -3,20 +3,47 @@ def extract_address_data(text):
     name = ''
     number = ''
     found_number = False
-    for part in parts:
-        if not found_number:
-            if part[0].isdigit():
-                found_number = True
-                number += part
+    address_types = ["no", "apt", "suite"] 
+    
+    parts = [part.replace(',', '') for part in parts]
+
+    for i, part in enumerate(parts):
+        if part.lower() not in address_types:
+            if parts[0][0].isdigit():
+                if part[0].isdigit():
+                    number += part
+                else:
+                    name += part + ' '
             else:
-                name += part + ' '
+                if not found_number:
+                    if part[0].isdigit():
+                        found_number = True
+                        number += part
+                    else:
+                        name += part + ' '
+                else:
+                    number += ' '
+                    number += part
         else:
-            number += ' '
-            number += part
+            name = name + number
+            number = ' '.join(parts[i:])
+            break
     
     return [name.strip(), number]
 
-examples = ["Miritiba 339", "Babaçu 500", "Cambuí 804B","Rio Branco 23", "Quirino dos Santos 23 b",]
+examples = [
+    "Miritiba 339", 
+    "Babaçu 500", 
+    "Cambuí 804B",
+    "Rio Branco 23", 
+    "Quirino dos Santos 23 b",
+    "4, Rue de la République", 
+    "100 Broadway Av", 
+    "Calle Sagasta, 26", 
+    "Calle 44 No 1991", 
+    "221B Baker Street",
+    
+]
 
 for example in examples:
     result = extract_address_data(example)
