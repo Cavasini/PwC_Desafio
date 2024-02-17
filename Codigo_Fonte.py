ec2b66c540969f70aa484b03d7186b6f4a49e399
cavasini <cavasini.dev@gmail.com>
2024-02-16 22:09:51 -0300

    Resolução Caso 02

--- Codigo_Fonte.py
+++ Codigo_Fonte.py
@@ -1,10 +1,22 @@
 def extract_address_data(text):
     parts = text.split()
-    name = parts[0]
-    number = parts[1]
-    return [name, number]
+    name = ''
+    number = ''
+    found_number = False
+    for part in parts:
+        if not found_number:
+            if part[0].isdigit():
+                found_number = True
+                number += part
+            else:
+                name += part + ' '
+        else:
+            number += ' '
+            number += part
+    
+    return [name.strip(), number]
 
-examples = ["Miritiba 339", "Babaçu 500", "Cambuí 804B"]
+examples = ["Miritiba 339", "Babaçu 500", "Cambuí 804B","Rio Branco 23", "Quirino dos Santos 23 b",]
 
 for example in examples:
     result = extract_address_data(example)
